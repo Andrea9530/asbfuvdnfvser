a6dffe698495f3bdc1554fe7a6f9ea38de347d60
Andrea9530 <65161045+Andrea9530@users.noreply.github.com>
2023-05-03 09:59:19 +0200

    Create test.s

---
+++ test.s
@@ -0,0 +1,51 @@
+.section .data
+
+hello:
+      .ascii "Ciao Mondo!\n"   #stringa costante
+
+hi:
+        .ascii "Hi\n"
+hello_len:
+      .long . - hello          #lunghezza della stringa in byte
+
+hi_len:
+        .long . - hi
+.section .text                 #sezione istruzioni
+      .global _start           #punto di inizio del programma
+
+_start:
+        movl $5, %eax
+        pushl %eax
+        popl %ebx
+        cmp $4,%ebx
+        jne fine
+      movl $4, %eax            #Carica il codice della system call WRITE
+                               #in eax per scrivere la stringa
+                               #”Ciao Mondo!” a video.
+
+      movl $1, %ebx            #Mette a 1 il contenuto di EBX
+                               #Quindi ora EBX=1. 1 è il
+                               #primo parametro per la write e
+                               #serve per indicare che vogliamo
+                               #scrivere nello standard output
+
+      leal hello, %ecx         #Secondo parametro dell write
+                               #Carica in ECX l’indirizzo di
+                               #memoria associato all’etichetta
+                               #hello, ovvero il puntatore alla
+                               #stringa “Ciao Mondo!\n” da stampare.
+
+      movl hello_len, %edx     #Terzo parametro della write
+                               #carica in EDX la lunghezza della
+                               #stringa “Ciao Mondo!\n”.
+
+      int $0x80                #esegue la system call write
+                               #tramite l’interrupt 0x80
+fine:
+      movl $1, %eax            #Mette a 1 il registro EAX
+                               #1 è il codice della system call exit
+
+      xorl %ebx, %ebx          #azzera EBX. Contiene il codice di
+                               #ritorno della exit
+
+      int $0x80                #esegue la system call exit
